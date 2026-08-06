d07dd93f61076f127ca1b832703ae2297c224337
WannProject <awankurniawan7889@gmail.com>
2026-08-06 21:10:55 +0800

    refactor: redirect home route to login

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\TicketStatusController;
 use App\Http\Middleware\EnsureTeamMembership;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::redirect('/', '/login')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
